8a21f13a720510f264df9c148507f1f0ea79876c
N3dal <nedalxzo@gmail.com>
2022-05-25 13:10:21 +0200

    import required modules

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 # -----------------------------------------------------------------
-#
+# this project is one of the 50 python project challenge.
 #
 #
 #
 # Author:N84.
 #
-# Create Date:Wed May 25 13:08:06 2022.
+# Create Date:Sun Oct 10 17:22:22 2021.
 # ///
 # ///
 # ///
 # -----------------------------------------------------------------
+
+
+# NOTE!!!:
+# My English language is not great, don't be surprised if you find something strange or a new word.
+
+
+from random import randint
+from os import system
+from os import name as OS_NAME
+from time import sleep as delay
+
+
